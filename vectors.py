8ff4cb246086743e35b508f4a65af1052fab58d9
kanandian <893979892@qq.com>
2019-12-03 14:26:24 +0800

    update

--- vectors.py
+++ vectors.py
@@ -5,12 +5,24 @@ class Vector:
     def draw(self):
         pass
 
+    def selected(self): # 被选中时的处理逻辑
+        # 找几个关键点，画出正方形表示选中状态
+        pass
+
+    def unselected(self):   # 失去选中状态时的处理逻辑
+        # 取到选中状态时画的正方形
+        pass
+
 
 class Line(Vector):
     pass
 
 
 class Rectangle(Vector):
+    pass
+
+
+class Square(Rectangle):
     pass
 
 
