6c6d00c7aba5c98f8ec2b1fa9ed0ecc09bda8d67
kanandian <893979892@qq.com>
2019-12-03 14:21:25 +0800

    update

---
+++ vectors.py
@@ -0,0 +1,26 @@
+import PyQt5
+
+
+class Vector:
+    def draw(self):
+        pass
+
+
+class Line(Vector):
+    pass
+
+
+class Rectangle(Vector):
+    pass
+
+
+class Ellipse(Vector):
+    pass
+
+
+class Circle(Ellipse):
+    pass
+
+
+class FivePointedStar(Vector):
+    pass
